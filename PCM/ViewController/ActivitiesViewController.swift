0ff2768d8255d12ec6eccf161187c8dde404fe70
ortizfrancielly <ortizfrancielly@gmail.com>
2021-10-15 00:59:47 -0300

    Implementando Table View

--- PCM/ViewController/ActivitiesViewController.swift
+++ PCM/ViewController/ActivitiesViewController.swift
@@ -8,13 +8,30 @@
 import Foundation
 import UIKit
 
-class ActivitiesScreenViewController: UIViewController{
+class ActivitiesScreenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    private lazy var atividades: [String] = {
+        let atividades: [String] = ["Atividade 1", "Atividade 2", "Atividade 3", "Atividade 4", "Atividade 5", "Atividade 6"]
+        return atividades
+    }()
+    
+    let cellReuseIdentifier = "cell"
     
     private lazy var exitButton: UIBarButtonItem = {
         var exitButton = UIBarButtonItem()
         exitButton = UIBarButtonItem(title: "Sair", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutTap(_:)))
         exitButton.tintColor = .buttonColor
         return exitButton
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .clear
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.dataSource = self
+        table.delegate = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        return table
     }()
     
     private lazy var segmentedControlCustom: UISegmentedControl = {
@@ -38,7 +55,12 @@ class ActivitiesScreenViewController: UIViewController{
         [
             segmentedControlCustom.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height * 0.125),
             segmentedControlCustom.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            segmentedControlCustom.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15)
+            segmentedControlCustom.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            
+            tableView.topAnchor.constraint(equalTo: segmentedControlCustom.bottomAnchor, constant: 10),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
             
         ]
     }()
@@ -54,8 +76,7 @@ class ActivitiesScreenViewController: UIViewController{
         navigationItem.rightBarButtonItem = exitButton
         
         view.addSubview(segmentedControlCustom)
-        
-        
+        view.addSubview(tableView)
     }
     
     override func viewDidLayoutSubviews() {
@@ -79,4 +100,24 @@ class ActivitiesScreenViewController: UIViewController{
             break
         }
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = (self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+               
+               // set the text from the data model
+            cell.textLabel?.text = self.atividades[indexPath.row]
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+               
+               return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            print("Célula tocada - \(indexPath.row).")
+        }
 }
