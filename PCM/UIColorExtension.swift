828574eda889c26f7627c18548a36c32c5aabba3
ortizfrancielly <ortizfrancielly@gmail.com>
2021-10-15 00:09:19 -0300

    Implementando Segmented Control

--- PCM/UIColorExtension.swift
+++ PCM/UIColorExtension.swift
@@ -13,5 +13,6 @@ extension UIColor {
     static let yellowProt: UIColor? = #colorLiteral(red: 0.9871408343, green: 0.8499235511, blue: 0.5342681408, alpha: 1)
     static let blackProt: UIColor? = #colorLiteral(red: 0.2352941176, green: 0.2352941176, blue: 0.262745098, alpha: 1)
     static let bgColor: UIColor? = #colorLiteral(red: 0.9960023761, green: 0.996121347, blue: 0.9959618449, alpha: 1)
-    
+    static let buttonColor: UIColor? = #colorLiteral(red: 0.3642297089, green: 0.4714961648, blue: 0.5909925103, alpha: 1)
+    static let segmentedColor: UIColor? = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.04)
 }
--- PCM/ViewController/ActivitiesViewController.swift
+++ PCM/ViewController/ActivitiesViewController.swift
@@ -9,7 +9,74 @@ import Foundation
 import UIKit
 
 class ActivitiesScreenViewController: UIViewController{
+    
+    private lazy var exitButton: UIBarButtonItem = {
+        var exitButton = UIBarButtonItem()
+        exitButton = UIBarButtonItem(title: "Sair", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutTap(_:)))
+        exitButton.tintColor = .buttonColor
+        return exitButton
+    }()
+    
+    private lazy var segmentedControlCustom: UISegmentedControl = {
+        let segmentedControlCustom: UISegmentedControl
+        let items = ["Liberados", "Em execução", "Concluídos"]
+        segmentedControlCustom = UISegmentedControl(items: items)
+        segmentedControlCustom.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControlCustom.selectedSegmentIndex = 1
+        let xPostion:CGFloat = 10
+        let yPostion:CGFloat = 150
+        let elementWidth:CGFloat = 300
+        let elementHeight:CGFloat = 30
+        segmentedControlCustom.frame = CGRect(x: xPostion, y: yPostion, width: elementWidth, height: elementHeight)
+        segmentedControlCustom.selectedSegmentIndex = 1
+        segmentedControlCustom.backgroundColor = .segmentedColor
+        segmentedControlCustom.addTarget(self, action: #selector(self.segmentedValueChanged(_:)), for: .valueChanged)
+        return segmentedControlCustom
+    }()
+    
+    private lazy var constraints: [NSLayoutConstraint] = {
+        [
+            segmentedControlCustom.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height * 0.125),
+            segmentedControlCustom.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            segmentedControlCustom.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15)
+            
+        ]
+    }()
+    
     override func viewDidLoad() {
-        view.backgroundColor = .blue
+        super.viewDidLoad()
+        view.backgroundColor = .bgColor
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        self.navigationItem.title = "Atividades"
+        navigationItem.rightBarButtonItem = exitButton
+        
+        view.addSubview(segmentedControlCustom)
+        
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    @objc func logoutTap (_ sender: UIButton){
+        self.present(InitialScreenViewController(), animated: true)
+    }
+    
+    @objc func segmentedValueChanged(_ sender:UISegmentedControl!)
+    {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print ("Liberados")
+        case 1:
+            print ("Em execução")
+        case 2:
+            print ("Concluídos")
+        default:
+            break
+        }
     }
 }
