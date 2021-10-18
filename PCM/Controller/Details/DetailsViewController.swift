6d9cb0d38f829b54d90d3b3d696dd331abc40dff
ortizfrancielly <ortizfrancielly@gmail.com>
2021-10-18 14:04:39 -0300

    Criando a Details View Controller

---
+++ PCM/Controller/Details/DetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.swift
+//  PCM
+//
+//  Created by Francielly Cristina Ortiz Candido on 18/10/21.
+//
+
+import Foundation
+import UIKit
+
+internal class DetailsViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
