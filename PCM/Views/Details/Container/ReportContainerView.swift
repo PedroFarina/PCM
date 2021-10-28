2aff95befe196839db79a5960240b879adf297b2
cs-pedro-silveira <pedro.silveira@accenture.com>
2021-10-28 17:52:31 -0300

    Adds ReportCellView

---
+++ PCM/Views/Details/Container/ReportContainerView.swift
@@ -0,0 +1,13 @@
+//
+//  ReportContainerView.swift
+//  PCM
+//
+//  Created by pedro.silveira on 28/10/21.
+//
+
+import Foundation
+import UIKit
+
+final class ReportContainerView: UIStackView {
+    
+}
